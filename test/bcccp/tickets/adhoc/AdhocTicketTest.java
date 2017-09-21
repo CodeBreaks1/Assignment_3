33c4f23bb3fe3ad5730da880c236eb0a433799c7
Bobur <bobur.tursunov91@gmail.com>
2017-09-21 18:26:23 +1000

    adding before test

--- test/bcccp/tickets/adhoc/AdhocTicketTest.java
+++ test/bcccp/tickets/adhoc/AdhocTicketTest.java
@@ -1,10 +1,20 @@
 package bcccp.tickets.adhoc;
 
+import java.util.*;
+
 class AdhocTicketTest {
 
     static IAdhocTicket testAdhoc;
     static IAdhocTicketDAO idao;
     static IAdhocTicketFactory ifactory;
 
+    Logger logger = Logger.getLogger("Unit testing for AdHocTicket class");
+    private float charge;
+
+    @BeforeAll
+    static void before() {
+        testAdhoc = mock(AdhocTicket.class);
+        idao = spy(new AdhocTicketDAO(new AdhocTicketFactory()));
+    }
 
 }
