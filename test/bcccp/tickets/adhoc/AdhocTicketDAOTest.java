6f66bae7353b782a5145b46174743bdbe1845f22
Bobur <bobur.tursunov91@gmail.com>
2017-09-21 19:24:28 +1000

    Adding testing methods for AdhocTicketDAOTest

--- test/bcccp/tickets/adhoc/AdhocTicketDAOTest.java
+++ test/bcccp/tickets/adhoc/AdhocTicketDAOTest.java
@@ -2,4 +2,23 @@ package bcccp.tickets.adhoc;
 
 class AdhocTicketDAOTest {
 	
+    @BeforeEach
+    void setUp() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+
+    @Test
+    void createTicket() {
+    }
+
+    @Test
+    void findTicketByBarcode() {
+    }
+
+    @Test
+    void getCurrentTickets() {
+    }
 }
