bea68d1990ee42b6e4cf5054faaa99265183cbd4
Bobur <bobur.tursunov91@gmail.com>
2017-09-21 19:12:57 +1000

    Adding new method to the class

--- bcccp/tickets/adhoc/AdhocTicketFactory.java
+++ bcccp/tickets/adhoc/AdhocTicketFactory.java
@@ -16,4 +16,20 @@ public class AdhocTicketFactory implements IAdhocTicketFactory {
         return formatter.format(new Date().getTime()); // the string that is encoded (to a bar code)
     }
 
+    public String generateBarCode(int ticketNum, String entryDate) {
+
+        String prefix = "0041"; // hex representation of "A". Unicode: U+0041
+
+        String hexNum = Integer.toHexString(ticketNum);
+
+        String hexDate = null;
+        try {
+            hexDate = toHexadecimal(entryDate);
+        } catch (UnsupportedEncodingException e) {
+            e.printStackTrace();
+        }
+
+        return prefix + "\u002D" + hexNum + "\u002D" + hexDate;
+    }
+
 }
